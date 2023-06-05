fa7c350d658b28fb0f65c809658b823e44c740d4
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2023-06-05 18:42:30 +0430

    MIYO

--- src/routes/api/getMediaDetails/+server.ts
+++ src/routes/api/getMediaDetails/+server.ts
@@ -1,9 +1,9 @@
-import { OMDB_API_KEY } from '$env/static/private';
+// import { OMDB_API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }: { request: any }) {
 	const { title } = await request.json();
-	const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`;
+	const url = `http://www.omdbapi.com/?apikey=72361aa264de956767628b1f9e47618b&t=${title}`;
 
 	const res = await fetch(url);
 	const details = await res.json();
